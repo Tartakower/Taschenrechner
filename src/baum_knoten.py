3c4e9401bef9fa5839c0de119e7150cb041b9a7b
Polu Tartakower <phish@quantentunnel.de>
2025-10-25 22:10:56 +0200

    Rechenbaum

--- src/baum_knoten.py
+++ src/baum_knoten.py
@@ -1,11 +1,11 @@
 from __future__ import annotations
 from abc import ABC, abstractmethod
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 
 from operatoren import Operator
 
 @dataclass()
-class BaumKnoten(ABC):
+class Baumknoten(ABC):
 
     @abstractmethod
     def berechne_wert(self) -> float: pass
@@ -14,108 +14,144 @@ class BaumKnoten(ABC):
     def einfacher_text(self) -> str: pass
 
     @abstractmethod
-    def empfange_besucher(self, besucher: BaumKnoten_Visitor) -> None: pass
+    def empfange_besucher(self, besucher: Baumknoten_Visitor) -> None: pass
 
 
 @dataclass()
-class ZahlKnoten(BaumKnoten):
+class Zahlknoten(Baumknoten):
     
-    wert: float = 0.0
+    _wert: float = 0.0
+
+    @property
+    def wert(self) -> float:
+        return self._wert
 
     def __str__(self) -> str:
-        return str(self.wert)
+        return str(self._wert)
     
     def berechne_wert(self) -> float:
-        return self.wert
+        return self._wert
 
     def einfacher_text(self) -> str:
         return str(self)
 
-    def empfange_besucher(self, besucher: BaumKnoten_Visitor) -> None:
-        besucher.besuche_wert_knoten(self)
+    def empfange_besucher(self, besucher: Baumknoten_Visitor) -> None:
+        besucher.besuche_zahlknoten(self)
 
 
 @dataclass()
-class OperatorKnoten(BaumKnoten):
+class Operatorknoten(Baumknoten):
 
-    __operator: Operator
-    __linker_knoten: BaumKnoten
-    __rechter_knoten: BaumKnoten
+    _operator: Operator
+    _linker_knoten: Baumknoten
+    _rechter_knoten: Baumknoten
 
     def __init__(self, operator: Operator) -> None:
-        self.__operator = operator
-        if self.__operator.ist_punktrechnung():
-            self.__linker_knoten = ZahlKnoten(1.0)
-            self.__rechter_knoten = ZahlKnoten(1.0)
+        self._operator = operator
+        if self._operator.ist_punktrechnung():
+            self._linker_knoten = Zahlknoten(1.0)
+            self._rechter_knoten = Zahlknoten(1.0)
         else:
-            self.__linker_knoten = ZahlKnoten(0.0)
-            self.__rechter_knoten = ZahlKnoten(0.0)
+            self._linker_knoten = Zahlknoten(0.0)
+            self._rechter_knoten = Zahlknoten(0.0)
 
     @property
     def operator(self) -> Operator:
-        return self.__operator
+        return self._operator
     
     @property
-    def linker_knoten(self) -> BaumKnoten:
-        return self.__linker_knoten
+    def linker_knoten(self) -> Baumknoten:
+        return self._linker_knoten
     
     @linker_knoten.setter
-    def linker_knoten(self, knoten: BaumKnoten) -> None:
-        self.__linker_knoten = knoten
+    def linker_knoten(self, knoten: Baumknoten) -> None:
+        self._linker_knoten = knoten
 
     @property
-    def rechter_knoten(self) -> BaumKnoten:
-        return self.__rechter_knoten
+    def rechter_knoten(self) -> Baumknoten:
+        return self._rechter_knoten
     
     @rechter_knoten.setter
-    def rechter_knoten(self, knoten: BaumKnoten) -> None:
-        self.__rechter_knoten = knoten
+    def rechter_knoten(self, knoten: Baumknoten) -> None:
+        self._rechter_knoten = knoten
 
     def __str__(self) -> str:
-        return "(" + str(self.__linker_knoten) + " " + str(self.__operator) + " " + str(self.__rechter_knoten) + ")"
+        return "(" + str(self._linker_knoten) + " " + str(self._operator) + " " + str(self._rechter_knoten) + ")"
 
     def berechne_wert(self) -> float:
-        return self.__operator.berechne(self.__linker_knoten.berechne_wert(), self.__rechter_knoten.berechne_wert())
+        return self._operator.berechne(self._linker_knoten.berechne_wert(), self._rechter_knoten.berechne_wert())
     
     def einfacher_text(self) -> str:
-        return str(self.__operator)
+        return str(self._operator)
 
-    def empfange_besucher(self, besucher: BaumKnoten_Visitor) -> None:
-        besucher.besuche_operator_knoten(self)
+    def empfange_besucher(self, besucher: Baumknoten_Visitor) -> None:
+        besucher.besuche_operatorknoten(self)
 
 
 @dataclass()
-class VorzeichenKnoten(BaumKnoten):    
+class Vorzeichenknoten(Baumknoten):    
 
-    __innerer_knoten: BaumKnoten
+    _innerer_knoten: Baumknoten
 
-    def __init__(self, innerer_knoten: BaumKnoten = ZahlKnoten(0.0)):
-        self.__innerer_knoten = innerer_knoten
+    def __init__(self, innerer_knoten: Baumknoten = Zahlknoten(0.0)):
+        self._innerer_knoten = innerer_knoten
 
     @property
-    def innerer_knoten(self) -> BaumKnoten:
-        return self.__innerer_knoten
+    def innerer_knoten(self) -> Baumknoten:
+        return self._innerer_knoten
 
     def __str__(self) -> str:
-        return "-" + str(self.__innerer_knoten)
+        return "-" + str(self._innerer_knoten)
         
     def einfacher_text(self) -> str:
         return "~"
 
     def berechne_wert(self) -> float:
-        return -1 * self.__innerer_knoten.berechne_wert()
+        return -1 * self._innerer_knoten.berechne_wert()
 
-    def empfange_besucher(self, besucher: BaumKnoten_Visitor) -> None:
-        besucher.besuche_vorzeichen_knoten(self)
+    def empfange_besucher(self, besucher: Baumknoten_Visitor) -> None:
+        besucher.besuche_vorzeichenknoten(self)
 
 
-class BaumKnoten_Visitor(ABC):
+@dataclass()
+class Rechenbaum(Baumknoten):
+    
+    _wurzel: Baumknoten
+
+    def __init__(self, wurzel: Baumknoten = Zahlknoten(0.0)):
+        self._wurzel = wurzel
+    
+    @property
+    def wurzel(self) -> Baumknoten:
+        return self._wurzel
+    
+    @wurzel.setter
+    def wurzel(self, neuer_knoten: Baumknoten) -> None:
+        self._wurzel = neuer_knoten
+    
+    def berechne_wert(self) -> float:
+        return self._wurzel.berechne_wert()
+    
+    def einfacher_text(self) -> str:
+        return self._wurzel.einfacher_text()
+    
+    def __str__(self) -> str:
+        return str(self._wurzel)
+    
+    def empfange_besucher(self, besucher: Baumknoten_Visitor) -> None:
+        besucher.besuche_rechenbaum(self)
+
+
+class Baumknoten_Visitor(ABC):
 
     @abstractmethod
-    def besuche_operator_knoten(self, knoten: OperatorKnoten) -> None: pass
+    def besuche_operatorknoten(self, knoten: Operatorknoten) -> None: pass
+    
+    @abstractmethod
+    def besuche_rechenbaum(self, knoten: Rechenbaum) -> None: pass
 
     @abstractmethod
-    def besuche_vorzeichen_knoten(self, knoten: VorzeichenKnoten) -> None: pass
+    def besuche_vorzeichenknoten(self, knoten: Vorzeichenknoten) -> None: pass
 
     @abstractmethod
-    def besuche_wert_knoten(self, knoten: ZahlKnoten) -> None: pass
+    def besuche_zahlknoten(self, knoten: Zahlknoten) -> None: pass
--- test/test_baum_knoten.py
+++ test/test_baum_knoten.py
@@ -1,55 +1,89 @@
-
-from baum_knoten import OperatorKnoten, ZahlKnoten, VorzeichenKnoten
-from operatoren import Operator
+import pytest
+from src.baum_knoten import Operatorknoten, Zahlknoten, Vorzeichenknoten, Rechenbaum
+from src.operatoren import Operator
 
 def test_zahl_knoten_wert():
-    knoten = ZahlKnoten(42.0)
+    knoten = Zahlknoten(42.0)
     assert knoten.berechne_wert() == 42.0
     assert str(knoten) == "42.0"
 
 def test_operator_addition():
-    knoten = OperatorKnoten(Operator.PLUS)
-    knoten.linker_knoten = ZahlKnoten(5.0)
-    knoten.rechter_knoten = ZahlKnoten(3.0)
+    knoten = Operatorknoten(Operator.PLUS)
+    knoten.linker_knoten = Zahlknoten(5.0)
+    knoten.rechter_knoten = Zahlknoten(3.0)
     assert knoten.berechne_wert() == 8.0
     assert str(knoten) == "(5.0 + 3.0)"
 
 def test_operator_multiplikation():
-    knoten = OperatorKnoten(Operator.MULT)
-    knoten.linker_knoten = ZahlKnoten(4.0)
-    knoten.rechter_knoten = ZahlKnoten(2.0)
+    knoten = Operatorknoten(Operator.MULT)
+    knoten.linker_knoten = Zahlknoten(4.0)
+    knoten.rechter_knoten = Zahlknoten(2.0)
     assert knoten.berechne_wert() == 8.0
     assert str(knoten) == "(4.0 * 2.0)"
 
 def test_verschachtelte_berechnung():
-    additionsknoten = OperatorKnoten(Operator.PLUS)
-    additionsknoten.linker_knoten = ZahlKnoten(1.0)
-    additionsknoten.rechter_knoten = ZahlKnoten(2.0)
+    additionsknoten = Operatorknoten(Operator.PLUS)
+    additionsknoten.linker_knoten = Zahlknoten(1.0)
+    additionsknoten.rechter_knoten = Zahlknoten(2.0)
     
-    multiplikationsknoten = OperatorKnoten(Operator.MULT)
+    multiplikationsknoten = Operatorknoten(Operator.MULT)
     multiplikationsknoten.linker_knoten = additionsknoten
-    multiplikationsknoten.rechter_knoten = ZahlKnoten(3.0)
+    multiplikationsknoten.rechter_knoten = Zahlknoten(3.0)
     
     assert multiplikationsknoten.berechne_wert() == 9.0
     assert str(multiplikationsknoten) == "((1.0 + 2.0) * 3.0)"
 
 def test_vorzeichen_knoten():
-    innerer = ZahlKnoten(5.0)
-    knoten = VorzeichenKnoten(innerer)
+    innerer = Zahlknoten(5.0)
+    knoten = Vorzeichenknoten(innerer)
     assert knoten.berechne_wert() == -5.0
     assert str(knoten) == "-5.0"
     assert knoten.einfacher_text() == "~"
 
-    knoten = VorzeichenKnoten()
+    knoten = Vorzeichenknoten()
     assert knoten.berechne_wert() == -0.0
     assert str(knoten) == "-0.0"
 
 
 def test_vorzeichen_mit_operator():
-    additionsknoten = OperatorKnoten(Operator.PLUS)
-    additionsknoten.linker_knoten = ZahlKnoten(3.0)
-    additionsknoten.rechter_knoten = ZahlKnoten(2.0)
+    additionsknoten = Operatorknoten(Operator.PLUS)
+    additionsknoten.linker_knoten = Zahlknoten(3.0)
+    additionsknoten.rechter_knoten = Zahlknoten(2.0)
     
-    vorzeichenknoten = VorzeichenKnoten(additionsknoten)
+    vorzeichenknoten = Vorzeichenknoten(additionsknoten)
     assert vorzeichenknoten.berechne_wert() == -5.0
     assert str(vorzeichenknoten) == "-(3.0 + 2.0)"
+
+
+def test_rechenbaum_standard():
+    baum = Rechenbaum()
+    assert baum.berechne_wert() == 0.0
+    assert str(baum) == "0.0"
+
+def test_rechenbaum_set_wurzel():
+    baum = Rechenbaum()
+    neuer_knoten = Zahlknoten(42.0)
+    baum.wurzel = neuer_knoten
+    assert baum.berechne_wert() == 42.0
+    assert str(baum) == "42.0"
+
+def test_rechenbaum_komplexer_ausdruck():
+    baum = Rechenbaum()
+    
+    # Baue (3 + 4) * 2
+    plus_knoten = Operatorknoten(Operator.PLUS)
+    plus_knoten.linker_knoten = Zahlknoten(3.0)
+    plus_knoten.rechter_knoten = Zahlknoten(4.0)
+    
+    mal_knoten = Operatorknoten(Operator.MULT)
+    mal_knoten.linker_knoten = plus_knoten
+    mal_knoten.rechter_knoten = Zahlknoten(2.0)
+    
+    baum.wurzel = mal_knoten
+    assert baum.berechne_wert() == 14.0
+    assert str(baum) == "((3.0 + 4.0) * 2.0)"
+    assert baum.einfacher_text() == "*"
+
+def test_rechenbaum_ohne_wurzel():
+    baum = Rechenbaum()
+    assert baum.berechne_wert() == 0.0
